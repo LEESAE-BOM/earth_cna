06901efddcf5eb566e40758fe24abd410002b04f
이새봄 <70834586+LEESAE-BOM@users.noreply.github.com>
2022-05-26 01:30:22 +0900

    Add files via upload

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:earth_cna/start.dart';
+import 'package:earth_cna/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
